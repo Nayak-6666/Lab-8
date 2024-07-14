189f72528374dde34fee7887f1cec5cc405f329c
Nayak-6666 <163091930+Nayak-6666@users.noreply.github.com>
2024-07-14 20:17:26 +0530

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "instance1" {
+  ami                         = "ami-0b72821e2f351e396"
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "Onekey"
+  vpc_security_group_ids     = ["sg-0e90ea394e18bea8f"]
+  subnet_id                   = "subnet-01b48833cdee67b0d"
+  associate_public_ip_address = true
+  user_data                   = "${file("data.sh")}"
+
+  tags = {
+    Name = "demoinstance"
+  }
+}
