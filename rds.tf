6995ba2e049f700cc7f202a792f7265e767b3c88
Nayak-6666 <163091930+Nayak-6666@users.noreply.github.com>
2024-07-17 01:21:20 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "instance1" {
   ami                         = "ami-0b72821e2f351e396"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.small"
   count                       = 1
   key_name                    = "Onekey"
-  vpc_security_group_ids     = ["sg-012290a9f94e53a19"]
+  vpc_security_group_ids     = ["sg-084466caf0b08812c"]
   subnet_id                   = "subnet-01b48833cdee67b0d"
   associate_public_ip_address = true
   user_data                   = "${file("data.sh")}"
